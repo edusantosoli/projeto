263e023d7a996d446036f79ee393d8f2b6a83f11
eduardo santos <edusantosoli@gmail.com>
2017-12-05 22:08:02 -0200

    criado pastas variavel

---
+++ variaveis/exemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variaveis exemplo 01</title>
+</head>
+	<body>
+			<?php
+			$nome = "Eduardo";
+			//echo $nome;
+			var_dump($nome);
+			?>
+	</body>
+</html>
+
+
+
+
---
+++ variaveis/exemplo02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tipo de Dados</title>
+</head>
+<body>
+
+	<?php
+
+		$nomecompleto = "Eduardo Oliveira Santos"
+		$anonasc = "1981"
+		$qtdfilhos = "2";
+		$estcivil = "casado";
+		$nomeconjunge = "Izabela Siqueira";
+
+		echo $nomecompleto; echo $anonasc; echo $estcivil;
+		?>
+
+
+</body>
+</html>
+
+
